b53e114fd2cbed5295d6a2c7e25bbbe700b1c50c
najafsikander <najafsikander@yahoo.com>
2025-01-02 16:45:24 +0400

    Configured ts for server and added common middlewares for server

---
+++ server/src/index.ts
@@ -0,0 +1,25 @@
+import express,{Express, Request, Response, urlencoded, json} from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import morgan from 'morgan';
+import helmet from "helmet";
+
+
+dotenv.config();
+
+const app:Express = express();
+const port = process.env.PORT || 4200;
+
+app.use(helmet());
+app.use(cors());
+app.use(morgan('combined'));
+app.use(urlencoded({extended: true}));
+app.use(json());
+
+app.get('/', (req:Request, res:Response) => {
+    res.send('Hello World');
+});
+
+app.listen(port, () => {
+    console.log('listening on port: ' + port);
+});
